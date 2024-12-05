1b05283a26d6e887db92481e4f49afc0476371f5
kusi <appaukusi05@gmail.com>
2024-12-05 14:58:21 +0000

    create game flow

---
+++ script.js
@@ -0,0 +1,83 @@
+function GameBoard() {
+    const rows = 3;
+    const columns = 3;
+    const gameboard = [];
+
+    for (let i = 0; i < rows; i++) {
+        gameboard[i] = [];
+        for (let j = 0; j < columns; j++) {
+            gameboard[i].push(Cell());
+        }
+    }
+
+    const getGameBoard = () => gameboard;
+
+    const chooseCell = (row, column, player) => {
+        if (row > rows || column > columns) return;
+        gameboard[row][column].setValue(player);
+    }
+
+    const printBoard = () => {
+        const boardWithCellValues = gameboard.map((row) => row.map((cell) => cell.getValue()));
+        console.log(boardWithCellValues);
+    }
+
+    return { getGameBoard, printBoard, chooseCell }
+}
+
+function Cell(){
+    let value = "-";
+
+    const setValue = (player) => {
+        value = player;
+    };
+
+    const getValue = () => value;
+
+    return { setValue, getValue }
+}
+
+
+function GameController(
+    player1 = "player One",
+    player2 = "player two"
+) {
+    const board = GameBoard();
+
+    const players = [
+        {
+            name: player1,
+            token: "x"
+        },
+        {
+            name: player2,
+            token: "o"
+        }
+    ];
+
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    }
+
+    const getActivePlayer = () => activePlayer;
+
+    const playNewRound = () => {
+        board.printBoard();
+        console.log(`${getActivePlayer().name}'s turn...`);
+    }
+
+    const playRound = (row, column) => {
+        board.chooseCell(row, column, getActivePlayer().token);
+
+        switchPlayerTurn();
+        playNewRound();
+    }
+    playNewRound();
+
+    return { playRound, getActivePlayer };
+}
+
+const game = GameController();
+
